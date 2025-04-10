1dac177fc6e6791971db7e7efaeaa131d59b2f6d
julia <juliakowaleski@ufl.edu>
2025-04-10 14:37:28 -0400

    this is good

--- calculator.py
+++ calculator.py
@@ -5,8 +5,18 @@ calculator.py
 One function per operation, in order.
 """
 
-
+import math
 def add(a, b): 
     return (a+b)
+def subtract(a,b):
+    return (a-b)
+def multiply(a,b):
+    return (a*b)
+def divide(a,b):
+    return (b/a)
+except ZeroDivisionError as e: #zerodivisionerror is already in python
+    print(str(e))
+except:
+    print("Something went wrong")
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,7 @@
 import unittest
 from calculator import *
 
+
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
