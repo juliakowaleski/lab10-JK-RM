883db1c1c1d194190f4331de25237e2add36990b
ronin <rmithaug@ufl.edu>
2025-04-10 15:04:16 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,24 +7,21 @@ One function per operation, in order.
 a = 10
 b = 1
 import math
-def add(a, b): 
-    return (a+b)
-def subtract(a,b):
-    return (a-b)
-def multiply(a,b):
-    return (a*b)
-def divide(a,b):
-    return (b/a)
-def divide(a,b):
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
     try:
-        return b/a
+        return a/b
     except ZeroDivisionError:
-        return 'division by zero'
-def logarithm(a,b):
+        return "Division by zero"
+def log(a, b):
     try:
-        log = math.log(b,a)
-        return log
-    except ValueError:
-        return 'logarithm failed'
-def exponent(a,b):
-    return a**b
+        math.log(b, a)
+    except:
+        return "Log error"
+def exp(a, b):
+    return a ** b
