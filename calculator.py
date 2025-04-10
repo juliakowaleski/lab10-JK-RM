9138523e44bfc868f1e3642e8f53b828341b9169
julia <juliakowaleski@ufl.edu>
2025-04-10 14:51:17 -0400

    v2

--- calculator.py
+++ calculator.py
@@ -4,7 +4,8 @@ calculator.py
 
 One function per operation, in order.
 """
-
+a = 10
+b = 1
 import math
 def add(a, b): 
     return (a+b)
@@ -14,9 +15,16 @@ def multiply(a,b):
     return (a*b)
 def divide(a,b):
     return (b/a)
-except ZeroDivisionError as e: #zerodivisionerror is already in python
-    print(str(e))
-except:
-    print("Something went wrong")
-
-
+def divide(a,b):
+    try:
+        return b/a
+    except ZeroDivisionError:
+        return 'division by zero'
+def logarithm(a,b):
+    try:
+        log = math.log(b,a)
+        return log
+    except ValueError:
+        return 'logarithm failed'
+def exponent(a,b):
+    return a**b
